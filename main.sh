15b5403b47b8f0194fdb04bf9f28385a93f8a14e
ferrreo <harderthanfire@gmail.com>
2024-02-21 23:54:45 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,11 +6,15 @@ DEBIAN_FRONTEND=noninteractive
 git clone https://github.com/KhronosGroup/Vulkan-Loader.git
 cd Vulkan-Loader
 git checkout v1.3.278
+cd ..
 mkdir vulkan-headers
-git clone https://github.com/KhronosGroup/Vulkan-Loader.git ./vulkan-headers
-cp -rvf ../debian ./
+git clone https://github.com/KhronosGroup/Vulkan-Headers.git ./vulkan-headers
 cd vulkan-headers
 git checkout v1.3.278
+cd ..
+cd Vulkan-Loader
+cp -rvf ../debian ./
+
 cd ..
 
 # Get build deps
